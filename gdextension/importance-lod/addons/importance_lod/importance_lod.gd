1618de3a6bc7e104f4438c2c582723a222b6346c
Arman Elgudzhyan <48544263+puchik@users.noreply.github.com>
2024-04-01 17:27:05 -0700

    Updates to plugin configuration

---
+++ gdextension/importance-lod/addons/importance_lod/importance_lod.gd
@@ -0,0 +1,14 @@
+@tool
+extends EditorPlugin
+const AUTOLOAD_NAME = "LODManagerAutoload"
+
+func _enter_tree():
+	# Initialization of the plugin goes here.
+	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/importance_lod/lod_manager.tscn")
+	pass
+
+
+func _exit_tree():
+	# Clean-up of the plugin goes here.
+	remove_autoload_singleton(AUTOLOAD_NAME)
+	pass
